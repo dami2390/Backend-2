f6a1eb337000337639c76c576f5bce84418c36d9
Damian <damian.e.ferrara@gmail.com>
2023-02-06 23:47:39 -0300

    correccion desafio 2

--- Entregables/desafio2.js
+++ Entregables/desafio2.js
@@ -1,8 +1,19 @@
-const fs = require('fs');
+import * as fs from "fs";
 
 class ProductManager {
-  constructor(path) {
-    this.path = path;
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.id = "";
+    this.products = [];
+    this.path = "./products.txt";
+  }
+
+  static addId() {
+    if (this.id) {
+      this.id++;
+    } else {
+      this.id = 1;
+    }
+    return this.id;
   }
 
   async addProduct(title, description, price, thumbnail, code, stock) {
@@ -10,7 +21,7 @@ class ProductManager {
       console.log("All fields are required");
       return;
     }
-  
+
     const products = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
     for (let i = 0; i < products.length; i++) {
       if (products[i].code === code) {
@@ -18,10 +29,9 @@ class ProductManager {
         return;
       }
     }
-  
-    this.idCounter++;
+
     const newProduct = {
-      id: this.idCounter,
+      id: ProductManager.addId(),
       title,
       description,
       price,
@@ -33,52 +43,106 @@ class ProductManager {
     await fs.promises.writeFile(this.path, JSON.stringify(products));
   }
 
-  async getProducts() {
+  getProducts = async () => {
+    let data = await fs.promises.readFile(this.path, "utf-8");
+    let salida = await JSON.parse(data);
+    this.products = salida;
+    return this.products;
+  };
+
+  /*  async getProducts() {
     try {
-      let data = await fs.readFile(this.path, 'utf-8');
+      let data = await fs.promises.readFile(this.path, 'utf-8');
       return JSON.parse(data);
     } catch (error) {
       console.error(error);
     }
-  }
+  } */
 
   async getProductById(id) {
     try {
       let products = await this.getProducts();
-      return products.find(product => product.id === id);
+      return products.find((product) => product.id === id);
     } catch (error) {
       console.error(error);
     }
   }
 
-  async updateProduct(id, productData) {
-    try {
-      let products = await this.getProducts();
-      let productIndex = products.findIndex(product => product.id === id);
-      products[productIndex] = { ...products[productIndex], ...productData };
-      await fs.writeFile(this.path, JSON.stringify(products));
-      return products[productIndex];
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  updateProductById = async (
+    id,
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock
+  ) => {
+    let updatedProd = { id, title, description, price, thumbnail, code, stock };
+    let cont = await fs.promises.readFile(this.path, "utf-8");
+    let aux = await JSON.parse(cont);
+    this.products = aux;
+    let updProdIndex = this.products.findIndex((product) => product.id === id);
+    this.products.splice(updProdIndex, 1);
+    this.products.push(updatedProd);
 
-  async deleteProduct(id) {
-    try {
-      let products = await this.getProducts();
-      let productIndex = products.findIndex(product => product.id === id);
-      let deletedProduct = products.splice(productIndex, 1);
-      await fs.writeFile(this.path, JSON.stringify(products));
-      return deletedProduct;
-    } catch (error) {
-      console.error(error);
+    let update = await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(this.products)
+    );
+  };
+
+  deleteProductById = async (id) => {
+    let cont = await fs.promises.readFile(this.path, 'utf-8')
+    let aux = await JSON.parse(cont)
+    this.products = aux
+    let delProdIndex = this.products.findIndex(product => product.id === id)
+    console.log(`The product with the following ID will be removed:: ${id}`),
+    this.products.splice(delProdIndex,1)
+
+    let update = await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+
     }
-  }
 }
 
-module.exports = ProductManager;
+const test = async () => {
+  await productManager.addProduct(
+    "product 1",
+    "descrip 1",
+    100,
+    "foto1",
+    "code1",
+    1
+  );
+  await productManager.addProduct(
+    "product 2",
+    "descrip 2",
+    200,
+    "foto2",
+    "code2",
+    2
+  );
+  await productManager.addProduct(
+    "product 3",
+    "descrip 3",
+    300,
+    "foto3",
+    "code3",
+    3
+  );
+};
 
-const test = new ProductManager("./ejemplo.txt");
-test.getProducts()
-/* test.addProduct("producto prueba", "este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-test.getProducts() */
+const productManager = new ProductManager();
+
+// Prueba 1: Cargar los productos al txt con test(). Luego comentar la función.
+test()
+
+//Prueba 2: Consulta general e individual con getProducts y getProductById.
+
+//console.table(await productManager.getProducts())
+//console.log(await productManager.getProductById(1))
+
+//Prueba 3: Editar un producto.
+//productManager.updateProductById(1, "product 1.1", "descrip 1.1", 1.1, "foto1.1", "code1.1", 1.1)
+
+//Prueba 4: Eliminar un producto.
+// productManager.deleteProductById(2)
--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ const users = [
 ]
 
 app.use(express.urlencoded({extended: true})) // Permite realizar consultas en la URL (req.query)
+app.use(express.json()); // Permite que le envie JSON
 
 app.get('/', (req, res) => {
     res.send("Este es mi primer servidor en Express")
@@ -56,6 +57,36 @@ app.get('/user/:idUser', (req, res) => {
     const user = users.find(user => user.id === parseInt(idUser))
     if(user){
         res.send(`Nombre de usuario ${user.apellido}` )
+    }else {
+        res.send(`No existe el user` )
+    }
+    
+})
+
+app.post('/user', (req, res) => {
+    let {nombre, apellido, cargo} = req.body
+    const indice = users.length //le mando uno más, para poder sumarlo
+    users.push({nombre: nombre, apellido: apellido, cargo: cargo, id: indice})
+    res.send("Usuario creado")
+    
+})
+
+app.put('/user/:id', (req, res) => { // actualiza todos los campos. si quiero modificar uno solo, metodo PATCH
+    let {nombre, apellido, cargo} = req.body
+    const indice = users.findIndex( usuario => usuario.id === parseInt(id)) 
+    users[indice].cargo = cargo
+    users[indice].apellido = apellido
+    users[indice].nombre = nombre
+    res.send("Usuario creado")
+    
+})
+
+app.delete('/user/:idUser', (req, res) => {
+    const idUser = req.params.idUser
+    const index = users.findIndex(user => user.id === parseInt(idUser))
+    if(index != -1){
+        users.splice(index, 1)
+        res.send(`Usuario eliminado` )
     }else {
         res.send(`No existe el user` )
     }
