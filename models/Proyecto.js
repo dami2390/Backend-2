ecc755d7d4cab9b5373d38d17a623a111cc92e4d
Damian <damian.e.ferrara@gmail.com>
2023-02-14 21:32:56 -0300

    ignore

--- models/Proyecto.js
+++
@@ -1,9 +0,0 @@
-class Proyecto {
-    constructor(nombre, cantDias, precioRelativo){
-        this.nombre = nombre;
-        this.cantDias = cantDias;
-        this.precioRelativo = precioRelativo;
-    }
-}
-
-export default Proyecto
